740f6d615244a456f0855b2326f5181be641e0a4
jn8416 <71154756+jn8416@users.noreply.github.com>
2023-01-22 19:38:23 -0500

    Add savescum

---
+++ savescum/main.js
@@ -0,0 +1,33 @@
+// Register mod
+Game.registerMod("savescum", { //this string needs to match the ID provided in your info.txt
+    init: function() {
+        //this function is called as soon as the mod is registered
+        let MOD = this;
+
+        //Add in save button
+        l('storeTitle').insertAdjacentHTML('beforeend', '<a style="font-size:12px;position:absolute;bottom:2px;right:2px;display:block;" class="smallFancyButton" id="savescumbutton">Save</a>');
+
+        //Click detector
+        AddEvent(l('savescumbutton'), 'click', function() {
+            if (event.key == "shift") {
+                Game.ImportSaveCode(MOD.savescum);
+                Game.CloseNotes();
+                Game.Notify('Loaded', '', [], 1);
+            } else {
+                MOD.savescum = Game.WriteSave(1);
+                Game.Notify('Saved', '', [], 1);
+            }
+        });
+
+        //note: this mod does nothing but show a notification at the bottom of the screen once it's loaded
+        Game.Notify("Save Scum Loaded", '', [16, 5]);
+    },
+    save: function() {
+        //use this to store persistent data associated with your mod
+        return String(this.savescum);
+    },
+    load: function(str) {
+        //do stuff with the string data you saved previously
+        this.savescum = str;
+    },
+});
