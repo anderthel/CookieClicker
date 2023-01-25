0e59ed0c8ce4f5c7dae496fbcd21096ee16c5616
jn8416 <71154756+jn8416@users.noreply.github.com>
2023-01-24 19:02:32 -0500

    Reduced repeat time

--- lumpname/main.js
+++ lumpname/main.js
@@ -7,20 +7,20 @@ Game.registerMod("lumpname", { //this string needs to match the ID provided in y
     setInterval(function() {
         if (Game.lumpCurrentType == 0) {
             // Normal
-            Game.Notify("Normal", '', [29, 14], 5);
+            Game.Notify("Normal", '', [29, 14], 0.5);
         } else if (Game.lumpCurrentType == 1) {
             // Bifurcated
-            Game.Notify("Bifurcated", '', [29, 15], 5);
+            Game.Notify("Bifurcated", '', [29, 15], 0.5);
         } else if (Game.lumpCurrentType == 2) {
             // Golden
-            Game.Notify("Golden", '', [29, 16], 5);
+            Game.Notify("Golden", '', [29, 16], 0.5);
         } else if (Game.lumpCurrentType == 3) {
             // Meaty
-            Game.Notify("Meaty", '', [29, 17], 5);
+            Game.Notify("Meaty", '', [29, 17], 0.5);
         } else if (Game.lumpCurrentType == 4) {
             // Caramelized
-            Game.Notify("Caramelized", '', [29, 27], 5);
+            Game.Notify("Caramelized", '', [29, 27], 0.5);
         }
-    }, 5000);
+    }, 500);
     }
 });
