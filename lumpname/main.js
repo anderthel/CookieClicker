d33c5799314499030e21d998d50fe49b8b7f06d3
jn8416 <71154756+jn8416@users.noreply.github.com>
2023-01-24 19:15:26 -0500

    Update main.js

--- lumpname/main.js
+++ lumpname/main.js
@@ -4,23 +4,23 @@ Game.registerMod("lumpname", { //this string needs to match the ID provided in y
         //this function is called as soon as the mod is registered
         let MOD = this;
 
-    setInterval(function() {
-        if (Game.lumpCurrentType == 0) {
-            // Normal
-            Game.Notify("Normal", '', [29, 14], 0.5);
-        } else if (Game.lumpCurrentType == 1) {
-            // Bifurcated
-            Game.Notify("Bifurcated", '', [29, 15], 0.5);
-        } else if (Game.lumpCurrentType == 2) {
-            // Golden
-            Game.Notify("Golden", '', [29, 16], 0.5);
-        } else if (Game.lumpCurrentType == 3) {
-            // Meaty
-            Game.Notify("Meaty", '', [29, 17], 0.5);
-        } else if (Game.lumpCurrentType == 4) {
-            // Caramelized
-            Game.Notify("Caramelized", '', [29, 27], 0.5);
-        }
-    }, 500);
+        setInterval(function() {
+            if (Game.lumpCurrentType == 0) {
+                // Normal
+                Game.Notify("Normal", '', [29, 14], 5);
+            } else if (Game.lumpCurrentType == 1) {
+                // Bifurcated
+                Game.Notify("Bifurcated", '', [29, 15], 5);
+            } else if (Game.lumpCurrentType == 2) {
+                // Golden
+                Game.Notify("Golden", '', [29, 16], 5);
+            } else if (Game.lumpCurrentType == 3) {
+                // Meaty
+                Game.Notify("Meaty", '', [29, 17], 5);
+            } else if (Game.lumpCurrentType == 4) {
+                // Caramelized
+                Game.Notify("Caramelized", '', [29, 27], 5);
+            }
+        }, 5000);
     }
 });
