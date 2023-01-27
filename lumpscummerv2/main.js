b4758015f091da29111f34aa24e4176ae6d8e8d8
jn8416 <71154756+jn8416@users.noreply.github.com>
2023-01-27 04:32:26 -0500

    Add files via upload

---
+++ lumpscummerv2/main.js
@@ -0,0 +1,34 @@
+// Register mod
+Game.registerMod("lumpscummerv2", { //this string needs to match the ID provided in your info.txt
+    init: function() {
+        //this function is called as soon as the mod is registered
+        let MOD = this;
+        MOD.goal = 2; /*the id of the type you want - 0 normal, 1 bi, 2 gold, 3 meat, 4 carmel*/
+        MOD.addition = 7; /*the amount you get for that type*/
+
+        // Save before starting
+        MOD.save = Game.WriteSave(1);
+
+        // Get information
+        MOD.count = Game.lumps;
+        MOD.old = Game.lumpCurrentType;
+
+        while (true) {
+            // Click the lump (will harvest if ready)
+            Game.clickLump;
+
+            if (Game.lumps == MOD.count) { /*If count hasnt increased (botched)*/
+                Game.ImportSaveCode(MOD.save);
+            } else if (MOD.old == MOD.goal && Game.lumps - MOD.addition != MOD.count) { /*If old is same as goal check correct amount gotten*/
+                Game.ImportSaveCode(MOD.save);
+            } else if (Game.lumpCurrentType == MOD.goal) { /*Check new lump type*/
+                Game.toSave = true;
+                Game.Notify('Done', '', [], 6000);
+                console.log(Game.WriteSave(1));
+                break;
+            } else {
+                Game.ImportSaveCode(MOD.save);
+            }
+        }
+    }
+});
