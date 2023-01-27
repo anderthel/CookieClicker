0606f1a3d4fb429db177cd9a58af66bc2502c419
jn8416 <71154756+jn8416@users.noreply.github.com>
2023-01-27 16:59:58 -0500

    Update main.js

--- autocast/main.js
+++ autocast/main.js
@@ -7,11 +7,24 @@ Game.registerMod("autocast", { //this string needs to match the ID provided in y
         // Spell to cast
         MOD.spell = "hand of fate";
 
+        // With buffs only
+        MOD.bufftoggle = true;
+
         function autocast() {
             MOD.wiz = Game.ObjectsById[7].minigame;
-            if (MOD.wiz.magic == MOD.wiz.magicM) {
-                MOD.wiz.castSpell(MOD.wiz.spells[MOD.spell]);
-                if (MOD.spell == "hand of fate") {
+            if (MOD.wiz.magic == MOD.wiz.magicM) {                                      /*if magic is full*/
+                if (MOD.bufftoggle == true) {                                           /*if toggle on do tests*/
+                    MOD.buffcount = 0;
+                    for (key in Game.buffs) {                                           /*count buffs*/
+                        MOD.buffcount++
+                    };
+                    if (MOD.buffcount >= 1) {                                           /*if at least 1 buff then cast*/
+                        MOD.wiz.castSpell(MOD.wiz.spells[MOD.spell]);
+                    }
+                } else {
+                    MOD.wiz.castSpell(MOD.wiz.spells[MOD.spell]);                       /*if toggle off cast*/
+                }
+                if (MOD.spell == "hand of fate") {                                      /*if hand of fate*/
                     Game.shimmers.forEach(function(shimmer) {
                         if ((shimmer.type == "golden") && (shimmer.wrath == 0)) {
                             shimmer.pop();
