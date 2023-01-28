9b0f1811e656cffd8c7896aa3fd236cbc07aeae0
jn8416 <71154756+jn8416@users.noreply.github.com>
2023-01-28 16:47:59 -0500

    Update main.js

--- autocast/main.js
+++ autocast/main.js
@@ -10,21 +10,35 @@ Game.registerMod("autocast", { //this string needs to match the ID provided in y
         // With buffs only
         MOD.bufftoggle = true;
 
+        // Button
+        l("grimoireContent").insertAdjacentHTML('beforeend', '<a style="font-size:12px;position:absolute;bottom:2px;right:2px;display:block;" class="smallFancyButton" id="autocastbutton">Cast: Off</a>');
+
+        // Button events
+        AddEvent(l('autocastbutton'), 'click', function() {
+            if (l('autocastbutton').innerText == "Cast: Off") {
+                l('autocastbutton').innerText = "Cast: On";
+                Game.registerHook("draw", autocast); /*Auto Cast*/
+            } else {
+                l('autocastbutton').innerText = "Cast: Off";
+                Game.removeHook('draw id', autocast);
+            }
+        })
+
         function autocast() {
             MOD.wiz = Game.ObjectsById[7].minigame;
-            if (MOD.wiz.magic == MOD.wiz.magicM) {                                      /*if magic is full*/
-                if (MOD.bufftoggle == true) {                                           /*if toggle on do tests*/
+            if (MOD.wiz.magic == MOD.wiz.magicM) { /*if magic is full*/
+                if (MOD.bufftoggle == true) { /*if toggle on do tests*/
                     MOD.buffcount = 1;
-                    for (key in Game.buffs) {                                           /*count buffs*/
+                    for (key in Game.buffs) { /*count buffs*/
                         MOD.buffcount++
                     };
-                    if (MOD.buffcount >= 1) {                                           /*if at least 1 buff then cast*/
+                    if (MOD.buffcount >= 1) { /*if at least 1 buff then cast*/
                         MOD.wiz.castSpell(MOD.wiz.spells[MOD.spell]);
                     }
                 } else {
-                    MOD.wiz.castSpell(MOD.wiz.spells[MOD.spell]);                       /*if toggle off cast*/
+                    MOD.wiz.castSpell(MOD.wiz.spells[MOD.spell]); /*if toggle off cast*/
                 }
-                if (MOD.spell == "hand of fate") {                                      /*if hand of fate*/
+                if (MOD.spell == "hand of fate") { /*if hand of fate*/
                     Game.shimmers.forEach(function(shimmer) {
                         if ((shimmer.type == "golden") && (shimmer.wrath == 0)) {
                             shimmer.pop();
@@ -37,11 +51,11 @@ Game.registerMod("autocast", { //this string needs to match the ID provided in y
         Game.registerHook("draw", autocast); /*Auto Cast*/
         Game.Notify("Auto Cast Loaded", '', [], 5);
     },
-    save:function(){
+    save: function() {
         //use this to store persistent data associated with your mod
         //note: as your mod gets more complex, you should consider storing a stringified JSON instead
     },
-    load:function(str){
+    load: function(str) {
         //do stuff with the string data you saved previously
     },
 });
