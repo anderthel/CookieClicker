3489873be80916b288e46cc438a9aa7f68a32447
jn8416 <71154756+jn8416@users.noreply.github.com>
2023-01-30 07:50:05 -0500

    Update main.js

--- stockmode/main.js
+++ stockmode/main.js
@@ -1,56 +1,52 @@
 // Register mod
-Game.registerMod("gardensave", { //this string needs to match the ID provided in your info.txt
+Game.registerMod("stockmode", { //this string needs to match the ID provided in your info.txt
     init: function() {
         //this function is called as soon as the mod is registered
         let MOD = this;
 
+        MOD.modeName = {
+                0: ['Stable', "128,128,128"],
+                1: ['Slow Rise', "0,153,0"],
+                2: ['Slow Fall', "128,30,128"],
+                3: ['Fast Rise', "0,200,0"],
+                4: ['Fast Fall', "128,10,128"],
+                5: ['Chaotic', "255, 0, 0"]
+            };
         // Button
-        l("gardenContent").insertAdjacentHTML('beforeend', '<a style="font-size:12px;position:absolute;bottom:2px;right:2px;display:block;" class="smallFancyButton" id="gardensavebutton">Scum</a>');
+        l("bankHeader").insertAdjacentHTML('beforeend', '<a style="font-size:12px;position:absolute;bottom:2px;right:2px;display:block;" class="smallFancyButton" id="stockmodebutton">Mode: Off</a>');
 
         // Button events
-        AddEvent(l('gardensavebutton'), 'click', function() {
-            savescum();
-        })
-
-        function savescum() {
-            MOD.savescum = Game.mods["savescum"].savescum;
-            MOD.farm = Game.ObjectsById[2].minigame;
-            MOD.locked = [];
-            MOD.tries = 0;
-            MOD.run = true;
-            // MOD.farm.nextStep = farm tick
-
-            // Get list of locked plants
-            MOD.farm.plantsById.forEach(function(plant) {
-                if (plant.unlocked == 0) {
-                    MOD.locked.push(plant.id);
-                }
-            });
-
-            // For each plot of farm check if a new plant is there
-            while (MOD.run) {
-                MOD.tries++; /*count*/
-
-                if (MOD.tries <= 50) {
-                    console.log("Try:" + String(MOD.tries).padStart(5, ' '))
-                    MOD.farm.plot.forEach(function(row) {
-                        row.forEach(function(plot) {
-                            if (MOD.locked.indexOf(plot) == -1) {
-                                Game.ImportSaveCode(MOD.savescum);
-                            } else {
-                                Game.CloseNotes();
-                                Game.Notify('Done', '', [], 1);
-                                MOD.run = false;
+        AddEvent(l('stockmodebutton'), 'click', function() {
+                if (l('stockmodebutton').innerText == "Mode: Off") {
+                    l('stockmodebutton').innerText = "Mode: On";
+                    MOD.printing = setInterval(function() {
+                        MOD.stocks = "";
+                        if (Game.ObjectsById[5].muted == 0) {
+                            Game.ObjectsById[5].minigame.goodsById.forEach(function(stock) {
+                                if (stock.hidden == false) {
+                                    MOD.stocks += '<h3 style="background-color:rgb(' + loc(MOD.modeName[stock.mode])[1] + ');">' + stock.symbol + ": " + loc(MOD.modeName[stock.mode])[0] + "</h3><br>";
+                                }
+                            });
+                            if (MOD.stocks != "") {
+                                Game.Notify(MOD.stocks, '', [], 5);
                             }
-                        });
-                    });
+                        }
+                    }, 5000);
                 } else {
-                    MOD.run = false;
-                    Game.Notify('Failed', '', [], 10);
+                    l('stockmodebutton').innerText = "Mode: Off";
+                    clearInterval(MOD.printing);
                 }
-            }
-        }
+            })
 
-        Game.Notify("Garden Save Loaded", '', [], 5);
-    }
+
+        // Loaded
+        Game.Notify("Stock Mode Loaded", '', [], 5);
+    },
+    save:function(){
+        //use this to store persistent data associated with your mod
+        //note: as your mod gets more complex, you should consider storing a stringified JSON instead
+    },
+    load:function(str){
+        //do stuff with the string data you saved previously
+    },
 });
